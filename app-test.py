ae6993eaa77216a892d1a8489657a76212c3fdea
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-06-27 16:00:31 +0300

    add functions to accept and save credentials

--- app-test.py
+++ app-test.py
@@ -1,5 +1,6 @@
 import unittest
 from app import User
+from app import Credentials
 
 class TestClass(unittest.TestCase):
     """
@@ -25,6 +26,36 @@ class TestClass(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
 
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('Instagram','Algun-chico','6*#@KingsleyG12')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Instagram')
+        self.assertEqual(self.new_credential.userName,'Algun-chico')
+        self.assertEqual(self.new_credential.password,'6*#@KingsleyG12')
 
+    def save_credential_test(self):
+        """
+        test case to test if the crential object is saved into the credentials list.
+        """
+        self.new_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
+
+    
 if __name__ == "__main__":
     unittest.main()
--- app.py
+++ app.py
@@ -21,4 +21,44 @@ class User:
         User.user_list.append(self)
     
 
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        '''
+        delete_account method deletes a  saved account from the list
+        '''
+        User.user_list.remove(self)
+
+class Credentials():
+    """
+    Create credentials class to help create new objects of credentials
+    """
+    credentials_list = []
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password
+    
+    def save_details(self):
+        """
+        method to store a new credential to the credentials list
+        """
+        Credentials.credentials_list.append(self)
+
    
